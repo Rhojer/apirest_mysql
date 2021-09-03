5635a6f66f522fd4a4af5801afda6913bc5e3cbf
DEScontrol <34425397+Rhojer@users.noreply.github.com>
2021-09-02 21:43:51 -0400

    2do commit powershell

--- routes/cursos.js
+++ routes/cursos.js
@@ -3,11 +3,30 @@ const ruta = express.Router();
 const validarToken = require('../middleware/auth.js')
 const conection = require('../config/conectSQL.js');
 
-ruta.post('/', validarToken, async(req,res) =>{
+ruta.post('/', validarToken, async(req, res) =>{
     const {nombre, descripcion } = req.body;
-    if(req.usuario.profesor = true){
+    await conection.query(`SELECT * FROM curso WHERE nombre = '${nombre}'`, (err, rows) =>{
+        console.log(rows.length)
+        if(rows.length == 1){
+            console.log('no1');
+           return res.json({msj:'curso ya existente'});
+       }else{
+            const respuesta= conection.query(`INSERT INTO curso(NOMBRE, DESCRIPCION, PROFESOR) VALUES('${nombre}','${descripcion}', '${req.usuario.nombre}')`, (err, rows) => {
+            //console.log(respuesta);
+            if(!err){
+                   res.json({
+                       msj:'curso creado satisfactoriamente',rows
+                   })
+               }else{
+                   res.status(400).json({msj:'error insertando datos del curso'})
+               }
+           })
+       }
+        
+    });
+  /*   if(req.usuario.profesor = true){
         await crearCurso(nombre, descripcion);
-    }
+    } */
 
 });
 
@@ -20,13 +39,15 @@ ruta.post('/', validarToken, async(req,res) =>{
 
 
  function crearCurso(nombre, descripcion){
-    conection.query(`SELECT * FROM curso WHERE nombre = '${nombre}'`, (err, rows) =>{
+    /* conection.query(`SELECT * FROM curso WHERE nombre = '${nombre}'`, (err, rows) =>{
         console.log(rows.length)
         if(rows.length = 1){
-           res.json({msj:'curso ya existente'})
+            console.log('no1');
+           return res.json({msj:'curso ya existente'});
        }else{
-           conection.query(`INSERT (NOMBRE, DESCRIPCION, AUTOR) INTO curso(${nombre},${descripcion}, ${req.usuario.id})`, (err, rows) => {
-               if(!err){
+            conection.query(`INSERT (NOMBRE, DESCRIPCION, AUTOR) INTO curso('${nombre}','${descripcion}', '${req.usuario.id}')`, (err, rows) => {
+            console.log('no');
+            if(!err){
                    res.json({
                        msj:'curso creado satisfactoriamente',rows
                    })
@@ -36,7 +57,7 @@ ruta.post('/', validarToken, async(req,res) =>{
            })
        }
         
-    });
+    }); */
     };
 
 
