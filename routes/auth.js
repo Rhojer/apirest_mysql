2dc933a20c5bed347c4666d9cedeb42600776ff0
DEScontrol <34425397+Rhojer@users.noreply.github.com>
2021-09-08 11:16:22 -0300

    password validate

--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,38 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const ruta = express.Router();
+const bcrypt = require('bcrypt');
 const conection = require('../config/conectSQL.js');
 
 const { env } = require('process');
 
 
-// consulta sql
-ruta.post('/', async( req, res) =>{
 
-        await conection.query(`SELECT * FROM usuario WHERE email = 'rhojer24@gmail.com';`, (err, rows) =>{
+ruta.post('/', async( req, res) =>{
+                // consulta sql
+        await conection.query(`SELECT * FROM usuario WHERE email = '${req.body.email}';`, (err, rows) =>{
             if(err){
                 const {sqlMessage: error} = err
                 res.status(400).json({msj:'error en la consulta a la base de datos: ', error})
-              console.log(err)
+
               conection.end();
             }
             else {
+                console.log(rows);
+                if(rows.length == 0){ return res.status(400).json({msj: 'usuario o password invalido'});}
                 let [{id, nombre, email, password, rol_id}] = rows;
+                     //validando password:
+                const passwordValidado = bcrypt.compareSync(req.body.password, password);
+                if(!passwordValidado){
+                    return res.status(400).json({msj: 'usuario o password invalido..'});
+                }
+                else{
+                    //creando el token de acceso:
                 const token = jwt.sign({datausuario: {id: id, nombre: nombre, email: email, password: password, rol: rol_id }
                 },'secret', { expiresIn: '1h' });
                 conection.end();
                 res.json(token);
-                console.log(rows);
-                console.log(rows.length)
-                
+                }
             }
 
         })
